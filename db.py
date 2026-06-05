a4057d7f5e7eea09892f529a517808421b4f47ae
Maria Gnativ <gnativ.shark@yandex.ru>
2026-06-05 20:23:28 +0300

    Update db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 DB_USER = os.getenv("MYSQL_USER")
 DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
-DB_HOST = os.getenv("MYSQL_HOST", "localhost")
+DB_HOST = os.getenv("MYSQL_HOST", "db")
 DB_NAME = os.getenv("MYSQL_DATABASE")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
